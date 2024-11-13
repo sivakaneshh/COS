79b26012c0db84f1c34ee74e7d553c52a77bbc7a
Raj051105 <70065317+Raj051105@users.noreply.github.com>
2024-11-14 00:04:38 +0530

    Changed Add food page to dispay food type in the existing food table

--- canteen/models.py
+++ canteen/models.py
@@ -10,7 +10,7 @@ class FoodItem(models.Model):
 
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    foodchoice = models.CharField(max_length=20, choices=choices, default='snack')
+    food_choice = models.CharField(max_length=20, choices=choices, default='snack')
     is_in_stock = models.BooleanField(default=True)
     stock_quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='food_images/', blank=True, null=True)
